2a0ab29b9f92ca13da2eedc2fd3edb99ae7f0677
Donaldc <39742608+DonaldChiweshe31@users.noreply.github.com>
2021-05-06 19:07:13 +0200

    Add files via upload
    Responsive Modern Login WinForm app with Register or login with social media apps

---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModernLoginFORM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+ 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            frm2.Show();
+        }
+
+        
+        Form2 frm2 = new Form2();
+       
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            picuser.BackgroundImage = Properties.Resources.user1;
+            panel1.BackColor = Color.FromArgb(78, 184, 206);
+            textBox1.ForeColor = Color.FromArgb(78, 184, 206);
+
+            
+            picpass.BackgroundImage = Properties.Resources.pass1;
+            panel2.ForeColor = Color.White;
+            textBox2.ForeColor = Color.White;
+
+           
+            picemail.BackgroundImage = Properties.Resources.email1;
+            panel3.ForeColor = Color.WhiteSmoke;
+            textBox3.ForeColor = Color.WhiteSmoke;
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            picemail.BackgroundImage = Properties.Resources.email1;
+            panel3.BackColor = Color.FromArgb(78, 184, 206);
+            textBox3.ForeColor = Color.FromArgb(7, 184, 206);
+
+            picuser.BackgroundImage = Properties.Resources.user1;
+            panel1.ForeColor = Color.WhiteSmoke;
+            textBox1.ForeColor = Color.WhiteSmoke;
+
+            picpass.BackgroundImage = Properties.Resources.pass1;
+            panel2.ForeColor = ForeColor = Color.WhiteSmoke;
+            textBox2.ForeColor = Color.WhiteSmoke;
+
+        }
+        
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            textBox2.PasswordChar = '*';
+            picpass.BackgroundImage = Properties.Resources.pass1;
+            panel2.BackColor = Color.FromArgb(78, 184, 206);
+            textBox2.ForeColor = Color.FromArgb(78, 184, 206);
+
+            picuser.BackgroundImage = Properties.Resources.user1;
+            panel1.BackColor = Color.WhiteSmoke;
+            textBox1.ForeColor = Color.WhiteSmoke;
+
+
+            picemail.BackgroundImage = Properties.Resources.email1;
+            panel3.BackColor = Color.WhiteSmoke;
+            textBox3.ForeColor = Color.WhiteSmoke;                  
+        }
+
+        private void picfb_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           // label2.Text = frm2.Left.ToString();
+            frm2.Left += 10;
+            if(frm2.Left >= 830)
+            {
+                timer1.Stop();
+                this.TopMost = false;
+                frm2.TopMost = true;
+                timer2.Start();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //label2.Text = frm2.Left.ToString();
+            frm2.Left -= 10;
+            if(frm2.Left <=525)
+            {
+                timer2.Stop();
+            }
+        } 
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace ModernLoginFORM
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.picpass = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.picuser = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picgp = new System.Windows.Forms.PictureBox();
+            this.pictw = new System.Windows.Forms.PictureBox();
+            this.picfb = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picpass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picuser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picgp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictw)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picfb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picpass
+            // 
+            this.picpass.Image = ((System.Drawing.Image)(resources.GetObject("picpass.Image")));
+            this.picpass.Location = new System.Drawing.Point(1, 264);
+            this.picpass.Name = "picpass";
+            this.picpass.Size = new System.Drawing.Size(33, 19);
+            this.picpass.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picpass.TabIndex = 12;
+            this.picpass.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(64, 289);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(250, 1);
+            this.panel2.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.ForeColor = System.Drawing.Color.Black;
+            this.textBox2.Location = new System.Drawing.Point(64, 257);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(250, 26);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.TabStop = false;
+            this.textBox2.Text = "Password";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(64, 227);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 1);
+            this.panel1.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(64, 195);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 26);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "Username";
+            // 
+            // picuser
+            // 
+            this.picuser.Image = ((System.Drawing.Image)(resources.GetObject("picuser.Image")));
+            this.picuser.Location = new System.Drawing.Point(1, 200);
+            this.picuser.Name = "picuser";
+            this.picuser.Size = new System.Drawing.Size(33, 19);
+            this.picuser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picuser.TabIndex = 7;
+            this.picuser.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(184)))), ((int)(((byte)(206)))));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.button1.Location = new System.Drawing.Point(64, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 53);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Login ";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(123, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 77);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // picgp
+            // 
+            this.picgp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picgp.Image = ((System.Drawing.Image)(resources.GetObject("picgp.Image")));
+            this.picgp.Location = new System.Drawing.Point(150, 453);
+            this.picgp.Name = "picgp";
+            this.picgp.Size = new System.Drawing.Size(48, 48);
+            this.picgp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picgp.TabIndex = 17;
+            this.picgp.TabStop = false;
+            // 
+            // pictw
+            // 
+            this.pictw.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictw.Image = ((System.Drawing.Image)(resources.GetObject("pictw.Image")));
+            this.pictw.Location = new System.Drawing.Point(242, 453);
+            this.pictw.Name = "pictw";
+            this.pictw.Size = new System.Drawing.Size(48, 48);
+            this.pictw.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictw.TabIndex = 16;
+            this.pictw.TabStop = false;
+            // 
+            // picfb
+            // 
+            this.picfb.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picfb.Image = ((System.Drawing.Image)(resources.GetObject("picfb.Image")));
+            this.picfb.Location = new System.Drawing.Point(64, 453);
+            this.picfb.Name = "picfb";
+            this.picfb.Size = new System.Drawing.Size(48, 48);
+            this.picfb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picfb.TabIndex = 15;
+            this.picfb.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(150, 412);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 16);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "login with Facebook";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(360, 605);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picgp);
+            this.Controls.Add(this.pictw);
+            this.Controls.Add(this.picfb);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picpass);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.picuser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "login with Facebook";
+            ((System.ComponentModel.ISupportInitialize)(this.picpass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picuser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picgp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictw)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picfb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picpass;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox picuser;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picgp;
+        private System.Windows.Forms.PictureBox pictw;
+        private System.Windows.Forms.PictureBox picfb;
+        private System.Windows.Forms.Label label1;
+    }
+}
